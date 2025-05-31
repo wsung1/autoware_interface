0cb026690ad6a89e6b766339bc757b754045b42c
wsung1 <wsung1@gmail.com>
2025-05-31 12:13:13 +0900

    feat: 기어단 중계, EPB 체결 등 1) velocity_status 토픽 publisher 이름 변경: can_message_handler(패키지명) -> can_message_receiver(노드명) steering_status 토픽 publisher 이름 변경: can_message_handler(패키지명) -> can_message_receiver(노드명)
    2)
    gear_status 중계
    /can_message_receiver/gear_state 토픽 수신
    /vehicle/status/gear_status 토픽 발행 to Autoware
    VCU - Autoware 간 gear_status 일치
    1 - 22 : P단
    2 - 20 : R단
    3 - 1 : N단
    4 - 2 : D단
    
    3)
    목적지(goal)에서 정차 중 EPB 체결을 위한 제동 지령
    /autoware_interface/force_brake 토픽 발행

--- include/autoware_interface/autoware_interface.hpp
+++ include/autoware_interface/autoware_interface.hpp
@@ -7,10 +7,12 @@
 #include "autoware_auto_vehicle_msgs/msg/control_mode_report.hpp"
 #include "autoware_auto_control_msgs/msg/ackermann_control_command.hpp"
 #include "autoware_auto_vehicle_msgs/msg/gear_command.hpp"
+#include "autoware_auto_vehicle_msgs/msg/gear_report.hpp"
 #include "std_msgs/msg/float64.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "can_msgs/msg/frame.hpp"
 #include "std_msgs/msg/int32.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
 
 #define KPH2MPS 1/3.6
 #define DEG2RAD 0.0174533
@@ -24,26 +26,40 @@ class AutowareInterface : public rclcpp::Node
     private:
         rclcpp::Subscription<autoware_auto_control_msgs::msg::AckermannControlCommand>::SharedPtr speed_angle_command_sub_;
         rclcpp::Subscription<autoware_auto_vehicle_msgs::msg::GearCommand>::SharedPtr gear_command_sub_;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_sub;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr ego_pose_sub;
         rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr speed_status_sub_;
         rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr angle_status_sub_;
+        rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr gear_status_sub_;
         
         rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr speed_command_pub_;
         rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr angle_command_pub_;
         rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr gear_command_pub_;
+        rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr force_brake_pub_;
         rclcpp::Publisher<autoware_auto_vehicle_msgs::msg::VelocityReport>::SharedPtr velocity_status_pub_;
         rclcpp::Publisher<autoware_auto_vehicle_msgs::msg::SteeringReport>::SharedPtr steering_tire_status_pub_;
         rclcpp::Publisher<autoware_auto_vehicle_msgs::msg::ControlModeReport>::SharedPtr control_mode_pub_;
+        rclcpp::Publisher<autoware_auto_vehicle_msgs::msg::GearReport>::SharedPtr gear_status_pub_;
+        
         rclcpp::TimerBase::SharedPtr timer_;
 
         double speed_command_ = 0.0;
         double angle_command_ = 0.0;
         int gear_command_ = 0;
+        double force_brake_ = 0.0;
+        int gear_status_ = 1;
         double steering_angle_ = 0.0;
         double vehicle_speed_ = 0.0;
+        geometry_msgs::msg::Pose goal_pose_;
+        double dist_to_goal_ = 0.0;
 
         void VelocityStatusCallback(const std_msgs::msg::Float64::SharedPtr msg);
         void SteeringStatusCallback(const std_msgs::msg::Float64::SharedPtr msg);
+        void GearStatusCallback(const std_msgs::msg::Int32::SharedPtr msg);
+
         void AWControlCommandCallback(const autoware_auto_control_msgs::msg::AckermannControlCommand::SharedPtr msg);
         void AWGearCommandCallback(const autoware_auto_vehicle_msgs::msg::GearCommand::SharedPtr msg);
+        void GoalPoseCallback(const geometry_msgs::msg::PoseStamped msg);
+        void EgoPoseCallback(const geometry_msgs::msg::PoseStamped msg);
         void TimerCallback();
 };
--- src/autoware_interface.cpp
+++ src/autoware_interface.cpp
@@ -9,21 +9,25 @@ AutowareInterface::AutowareInterface() : Node("autoware_interface")
         "/control/command/control_cmd", rclcpp::QoS(1), std::bind(&AutowareInterface::AWControlCommandCallback, this, std::placeholders::_1));
     gear_command_sub_ = this->create_subscription<autoware_auto_vehicle_msgs::msg::GearCommand>(
         "/control/command/gear_cmd", rclcpp::QoS(1), std::bind(&AutowareInterface::AWGearCommandCallback, this, std::placeholders::_1));
-    
+    goal_pose_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+        "/planning/mission_planning/echo_back_goal_pose", rclcpp::QoS{1}.transient_local(), std::bind(&AutowareInterface::GoalPoseCallback, this, std::placeholders::_1));
+    ego_pose_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+        "/localization/pose_twist_fusion_filter/pose", rclcpp::QoS(1), std::bind(&AutowareInterface::EgoPoseCallback, this, std::placeholders::_1));
+
     /* From CANMessageHandler */
     speed_status_sub_ = this->create_subscription<std_msgs::msg::Float64>(
-        "/can_message_handler/velocity_status", rclcpp::QoS(1), std::bind(&AutowareInterface::VelocityStatusCallback, this, std::placeholders::_1));
+        "/can_message_receiver/velocity_status", rclcpp::QoS(1), std::bind(&AutowareInterface::VelocityStatusCallback, this, std::placeholders::_1));
     angle_status_sub_ = this->create_subscription<std_msgs::msg::Float64>(
-        "/can_message_handler/steering_status", rclcpp::QoS(1), std::bind(&AutowareInterface::SteeringStatusCallback, this, std::placeholders::_1));
-
+        "/can_message_receiver/steering_status", rclcpp::QoS(1), std::bind(&AutowareInterface::SteeringStatusCallback, this, std::placeholders::_1));
+    gear_status_sub_ = this->create_subscription<std_msgs::msg::Int32>(
+        "/can_message_receiver/gear_state", rclcpp::QoS(1), std::bind(&AutowareInterface::GearStatusCallback, this, std::placeholders::_1));
 
     /* To TwistController */
     speed_command_pub_ = this->create_publisher<std_msgs::msg::Float64>("/autoware_interface/velocity_cmd", rclcpp::QoS(1)); 
     angle_command_pub_ = this->create_publisher<std_msgs::msg::Float64>("/autoware_interface/steering_cmd", rclcpp::QoS(1));
-
-    /* To CANMessageHandler */
     gear_command_pub_ = this->create_publisher<std_msgs::msg::Int32>("/autoware_interface/gear_cmd", rclcpp::QoS(1));
-    
+    force_brake_pub_ = this->create_publisher<std_msgs::msg::Float64>("/autoware_interface/force_brake", rclcpp::QoS(1));
+
     /* To Autoware */
     velocity_status_pub_ = this->create_publisher<autoware_auto_vehicle_msgs::msg::VelocityReport>(
         "/vehicle/status/velocity_status", rclcpp::QoS(1));
@@ -31,6 +35,8 @@ AutowareInterface::AutowareInterface() : Node("autoware_interface")
         "/vehicle/status/steering_status", rclcpp::QoS(1));
     control_mode_pub_ = this->create_publisher<autoware_auto_vehicle_msgs::msg::ControlModeReport>(
         "/vehicle/status/control_mode", rclcpp::QoS(1));
+    gear_status_pub_ = this->create_publisher<autoware_auto_vehicle_msgs::msg::GearReport>(
+        "/vehicle/status/gear_status", rclcpp::QoS(1));
 
     timer_ = this->create_wall_timer(10ms, std::bind(&AutowareInterface::TimerCallback, this));
 }
@@ -56,6 +62,23 @@ void AutowareInterface::AWGearCommandCallback(const autoware_auto_vehicle_msgs::
     gear_command_ = msg->command;
 }
 
+void AutowareInterface::GoalPoseCallback(const geometry_msgs::msg::PoseStamped msg)
+{
+    goal_pose_ = msg.pose;
+}
+
+void AutowareInterface::EgoPoseCallback(const geometry_msgs::msg::PoseStamped msg)
+{
+    geometry_msgs::msg::Pose ego_pose = msg.pose;
+    dist_to_goal_ = std::hypot(ego_pose.position.x - goal_pose_.position.x, ego_pose.position.y - goal_pose_.position.y);
+    RCLCPP_INFO(this->get_logger(), "distance : %f", dist_to_goal_);
+}
+
+void AutowareInterface::GearStatusCallback(const std_msgs::msg::Int32::SharedPtr msg)
+{
+    gear_status_ = msg->data;
+}
+
 void AutowareInterface::TimerCallback()
 {    
     /* To Autoware */
@@ -74,23 +97,71 @@ void AutowareInterface::TimerCallback()
     autoware_auto_vehicle_msgs::msg::ControlModeReport control_mode_msg;
     control_mode_msg.mode = 1;
     control_mode_pub_->publish(control_mode_msg);
-    
+
+    autoware_auto_vehicle_msgs::msg::GearReport gear_status_msg;
+    if(gear_status_ == 1) gear_status_ = 22; //P
+    else if(gear_status_ == 2) gear_status_ = 20; //R
+    else if(gear_status_ == 3) gear_status_ = 1; //N
+    else if(gear_status_ == 4) gear_status_ = 2; //D
+    else gear_status_ = 22; //P
+    gear_status_msg.report = gear_status_;
+    gear_status_pub_->publish(gear_status_msg);
 
     /* To TwistController */
     std_msgs::msg::Float64 speed_command_msg;
+    // float custom_mps_cmd = static_cast<int>(speed_command_ * 3.6) / 3.6; //HJK_250423
     speed_command_msg.data = speed_command_;
+    // speed_command_msg.data = custom_mps_cmd;
     speed_command_pub_->publish(speed_command_msg);
 
     std_msgs::msg::Float64 angle_command_msg;
     angle_command_msg.data = angle_command_; // * 15.7; // No steering wheel in KAMO, thus no steering wheel angle, only tire angle
     angle_command_pub_->publish(angle_command_msg);
 
-
-    /* To CANMessageHandler */
     std_msgs::msg::Int32 gear_command_msg;
     gear_command_msg.data = static_cast<int32_t>(gear_command_);
-    gear_command_pub_->publish(gear_command_msg);
+    
+    std_msgs::msg::Float64 force_brake_msg;
 
+
+    // if(gear_command_ == 22)
+    // {
+    //     if(force_brake_ == -1000.0)
+    //     {
+    //         gear_command_pub_->publish(gear_command_msg);
+    //     }
+    //     force_brake_msg.data = force_brake_;
+    //     force_brake_pub_->publish(force_brake_msg);
+    // }
+    // else
+    // {
+    //     force_brake_ = 0.0;
+    //     force_brake_msg.data = force_brake_;
+    //     force_brake_pub_->publish(force_brake_msg);
+    //     gear_command_pub_->publish(gear_command_msg);
+    // }
+
+    if(dist_to_goal_ < 3.0) //start force brake distance [m]
+    {
+        force_brake_ -= 1.5; //increment gain
+        if(force_brake_ <= -1000.0)
+        {
+            if(gear_command_ == 22)
+            {
+                gear_command_pub_->publish(gear_command_msg);
+            }
+            force_brake_ = -1000.0;
+        }
+        force_brake_msg.data = force_brake_;
+        force_brake_pub_->publish(force_brake_msg); 
+    }
+    else
+    {
+        force_brake_ = 0.0;
+        force_brake_msg.data = force_brake_;
+        force_brake_pub_->publish(force_brake_msg);
+        gear_command_pub_->publish(gear_command_msg);
+    }
 }
 
 int main(int argc, char **argv) 
